2079e447874c5fee3b02df55a218ac30cd29d143
Marek Paska <paskma@gmail.com>
2012-07-15 22:42:36 +0200

    Fixed typos in pasys compile script

--- binding/c/compile.sh
+++ binding/c/compile.sh
@@ -11,7 +11,8 @@ rm -f libpasys.so.1.0.1
 gcc -static client.c -L. -lpasys -lm -o client_static -lpthread
 echo dynamic lib
 gcc -c -fPIC pasys.c -o pasys.o
-gcc -c -fPIC simplenet.c -o symplenet.o
+gcc -c -fPIC simplenet.c -o simplenet.o
+gcc -c -fPIC simplecon.c -o simplecon.o
 gcc -shared -lm -Wl,-soname,libpasys.so.1 -o libpasys.so.1.0.1  pasys.o simplenet.o simplecon.o
 echo dyn prg
 rm libpasys.a
