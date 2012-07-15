b60ea50ff955571bb7fd469bf144a4a920630c61
Marek Paska <paskma@gmail.com>
2012-07-15 11:48:09 +0200

    Jasmin script moved to bin.

--- environment.sh
+++ environment.sh
@@ -2,7 +2,8 @@ PARLIB_FRAMEWORK_ROOT="/home/paskma/projects/pypygit/pypy/translator/goal/framew
 PYPY_ROOT="/home/paskma/projects/pypygit"
 # this interpreter is used for compilation, not for "pure" run
 PYTHON_BIN="/home/paskma/B/opt/python24/bin/python"
-JPF_ROOT=/home/paskma/projects/jpf_trunk2
+JPF_ROOT="/home/paskma/projects/jpf_trunk2"
+JASMIN_JAR="/home/paskma/B/opt/jasmin-2.2/jasmin.jar"
 
 
 TRANSLATE="$PYPY_ROOT/pypy/translator/goal/translate.py"
