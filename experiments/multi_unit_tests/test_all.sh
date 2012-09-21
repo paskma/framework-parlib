475a44238eee1c64fbaeb8dbf8f73fd8247127ba
Marek Paska <paskma@gmail.com>
2012-09-21 11:48:18 +0200

    Script for running multi-backend unit tests moved to bin.

--- experiments/multi_unit_tests/test_all.sh
+++ experiments/multi_unit_tests/test_all.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
 
-F="test_protocol.txt"
+par_compile_run_all.sh
 
-echo "==Test protocol `date`." > $F
-echo "==Pure Python:" >> $F
-par_run_pure.sh >> $F
-echo "==C:" >> $F
-par_compile_c.sh
-par_run_c.sh >> $F
-echo "==Java:" >> $F
-par_compile_j.sh
-par_run_j.sh >> $F
-echo "==Test done." >> $F
-cat $F
